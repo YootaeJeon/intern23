36b2ef592903c0d4899513cc2435a6b43467a3f1
jeon_yootae <jeon_lovely@kakao.com>
2023-07-17 13:29:12 +0900

    first commit

---
+++ T3Q/test.py
@@ -0,0 +1,17 @@
+print('git config --global user.name "jeon_yootae"')
+
+print('git config --global user.email "_@kakao.com"')
+
+print("git config --list")
+
+print("깃 테스트용")
+
+print("git init")
+
+print("git add.")
+
+print("git status")
+
+print('git commit -m "first commit"')
+      
+print("git init / ")
